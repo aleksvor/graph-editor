9708aecd3149c4f03226c8b4387429f3a246abf3
Alex Vorobyev <avorobyev@protonmail.com>
2018-12-08 12:39:28 +0300

    Начальный коммит

---
+++ GraphEditor/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GraphEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectButton = new System.Windows.Forms.Button();
+            this.deleteALLButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.drawEdgeButton = new System.Windows.Forms.Button();
+            this.drawVertexButton = new System.Windows.Forms.Button();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.sheet = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectButton
+            // 
+            this.selectButton.Location = new System.Drawing.Point(11, 19);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(100, 45);
+            this.selectButton.TabIndex = 14;
+            this.selectButton.Text = "Выделить";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // deleteALLButton
+            // 
+            this.deleteALLButton.Location = new System.Drawing.Point(12, 224);
+            this.deleteALLButton.Name = "deleteALLButton";
+            this.deleteALLButton.Size = new System.Drawing.Size(45, 45);
+            this.deleteALLButton.TabIndex = 13;
+            this.deleteALLButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(12, 173);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(99, 45);
+            this.deleteButton.TabIndex = 12;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // drawEdgeButton
+            // 
+            this.drawEdgeButton.Location = new System.Drawing.Point(11, 122);
+            this.drawEdgeButton.Name = "drawEdgeButton";
+            this.drawEdgeButton.Size = new System.Drawing.Size(100, 45);
+            this.drawEdgeButton.TabIndex = 11;
+            this.drawEdgeButton.Text = "Ребро";
+            this.drawEdgeButton.UseVisualStyleBackColor = true;
+            this.drawEdgeButton.Click += new System.EventHandler(this.drawEdgeButton_Click);
+            // 
+            // drawVertexButton
+            // 
+            this.drawVertexButton.Location = new System.Drawing.Point(12, 71);
+            this.drawVertexButton.Name = "drawVertexButton";
+            this.drawVertexButton.Size = new System.Drawing.Size(45, 45);
+            this.drawVertexButton.TabIndex = 10;
+            this.drawVertexButton.UseVisualStyleBackColor = true;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(612, 19);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(244, 288);
+            this.propertyGrid1.TabIndex = 15;
+            // 
+            // sheet
+            // 
+            this.sheet.BackColor = System.Drawing.Color.White;
+            this.sheet.Location = new System.Drawing.Point(117, 19);
+            this.sheet.Name = "sheet";
+            this.sheet.Size = new System.Drawing.Size(489, 415);
+            this.sheet.TabIndex = 16;
+            this.sheet.TabStop = false;
+            this.sheet.Click += new System.EventHandler(this.sheet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 482);
+            this.Controls.Add(this.sheet);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.deleteALLButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.drawEdgeButton);
+            this.Controls.Add(this.drawVertexButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button deleteALLButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button drawEdgeButton;
+        private System.Windows.Forms.Button drawVertexButton;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.PictureBox sheet;
+
+
+    }
+}
+
---
+++ GraphEditor/krug.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphEditor
+{
+    public partial class krug : Component
+    {
+        public krug()
+        {
+            InitializeComponent();
+        }
+
+        public krug(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}

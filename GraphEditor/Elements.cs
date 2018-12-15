d78083666ac5ab1ac2310331554a26838336876d
Alex Vorobyev <avorobyev@protonmail.com>
2018-12-15 14:05:22 +0300

    1. Рефакторинг 2. Добавлен gitignore.

--- GraphEditor/Elements.cs
+++ GraphEditor/Elements.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace GraphEditor
 {
-    class Elements
+    namespace Elements
     {
         class Vertex
         {
@@ -20,17 +20,25 @@ namespace GraphEditor
         }
         class Edge
         {
-            public float k, b;
+            public float k, b, x1, x2, y1, y2;
             public TextBox txtBoxFrom, txtBoxTo;
 
-            public Edge(float k, float b, TextBox txtBoxFrom, TextBox txtBoxTo)
+            public Edge(float k, float b,
+                float x1, float x2,
+                float y1, float y2,
+                TextBox txtBoxFrom, TextBox txtBoxTo)
             {
                 this.k = k;
                 this.b = b;
+                this.x1 = x1;
+                this.x2 = x2;
+                this.y1 = y1;
+                this.y2 = y2;
                 this.txtBoxFrom = txtBoxFrom;
                 this.txtBoxTo = txtBoxTo;
             }
         }
+
     }
 
 }
--- GraphEditor/Form1.cs
+++ GraphEditor/Form1.cs
@@ -16,8 +16,7 @@ using System.Xml.Serialization;
 using System.IO;
 using System.Xml.Linq;
 using System.Drawing.Drawing2D;
-
-
+using GraphEditor.Elements;
 
 namespace GraphEditor
 {
@@ -551,39 +550,6 @@ namespace GraphEditor
             }
             scrollValue = vScrollBar1.Value;
             reDrawAll();
-        }
-
-
-    }
-
-    public class Vertex
-    {
-        public TextBox txtBoxVertex;
-
-        public Vertex(TextBox txtBoxVertex)
-        {
-            this.txtBoxVertex = txtBoxVertex;
-        }
-    }
-
-    public class Edge
-    {
-        public float k, b, x1, x2, y1, y2;
-        public TextBox txtBoxFrom, txtBoxTo;
-
-        public Edge(float k, float b,
-            float x1, float x2,
-            float y1, float y2,
-            TextBox txtBoxFrom, TextBox txtBoxTo)
-        {
-            this.k = k;
-            this.b = b;
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-            this.txtBoxFrom = txtBoxFrom;
-            this.txtBoxTo = txtBoxTo;
         }
     }
 }
